956a4b54e9577905b3601f8cd7f44fd854e2c9f4
marco <marco@freepascal.org>
2003-08-28 14:06:18 +0000

    * Works, but keypress fails
    git-svn-id: @424 -

--- trunk/contrib/ics/examples/conping.dpr
+++ trunk/contrib/ics/examples/conping.dpr
@@ -1,8 +1,9 @@
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 Author:       François PIETTE
-Description:  This demo show how to use the TPing object to ping any host.
+Description:  This demo shows how to use the TPing object to ping any host.
 Creation:     November 30, 1997
+Destruction:  August 28, 2003
 Version:      1.03-ugly-hack-to-consoleapp-v3.1415
 EMail:        francois.piette@overbyte.be  http://www.overbyte.be
               francois.piette@rtfm.be      http://www.rtfm.be/fpiette
@@ -45,60 +46,63 @@ Dec 26, 1998 V1.02 Changed event handler for new TPing version (1.10)
 Nov 10, 2002 V1.03 Added Reply.Status in display when failed
                    Changed argument name from Error to Status in EchoReply
                    event (same change has in component).
+Aug 28, 2003 v1.03-ugly-hack-to-consoleapp-v3.1415
+                   (MarcoV) Small patch to console application
+                   Currently exit by pressing "any" key not working.
+                   (don't know how to do Delphi compat)
 
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
 Program conping;
-
 {$i icsdef.inc}
 
 uses
   Windows, Messages, SysUtils, Classes,
   Ping;
 
-Const PINGMEHOST='toad.stack.nl';
 
 type TPinger=Class(TComponent)
-    Ping1: TPing;
-    constructor Create(AOwner:TComponent);
-    procedure GoPingSomebody(Sender: TObject);
-    procedure Ping1Display(Sender: TObject; Icmp: TObject; Msg: String);
-    procedure Ping1DnsLookupDone(Sender: TObject; Error: Word);
-    procedure CancelButtonClick(Sender: TObject);
-    procedure Ping1EchoRequest(Sender: TObject; Icmp: TObject);
-    procedure Ping1EchoReply(Sender: TObject; Icmp: TObject; Status: Integer);
-  end;
+                   Private
+                     Ping1    : TPing;
+                     Hostname : String;
+                     DnsStatus: Boolean;
+                   public
+                     constructor Create(AOwner:TComponent); override;
+                     destructor Destroy; override;
+                     procedure ResolveAndPing(Const HostToPing:String);
+                     procedure Ping1Display(Sender: TObject; Icmp: TObject; Msg: String);
+                     procedure Ping1DnsLookupDone(Sender: TObject; Error: Word);
+                     procedure CancelButtonClick(Sender: TObject);
+                     procedure Ping1EchoRequest(Sender: TObject; Icmp: TObject);
+                     procedure Ping1EchoReply(Sender: TObject; Icmp: TObject; Status: Integer);
+                     procedure Ping;
+                     end;
 
 
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
-procedure TPinger.GoPingSomebody(Sender: TObject);
+procedure TPinger.ResolveAndPing(Const HostToPing:String);
 begin
-//  DisplayMemo.Clear;
-    writeln('Resolving host ''' + pingmehost + '''');
-//    PingButton.Enabled   := FALSE;
-//    CancelButton.Enabled := TRUE;
-
-    Ping1.DnsLookup(pingmehost);
+    HostName:=HostToPing;
+    writeln('Resolving host ''' + HostToPing + '''');
+    Ping1.DnsLookup(HostToPing);
 end;
 
 
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
 procedure TPinger.Ping1DnsLookupDone(Sender: TObject; Error: Word);
 
-var s : String;
 begin
-//    CancelButton.Enabled := FALSE;
-//    PingButton.Enabled   := TRUE;
 
     if Error <> 0 then begin
-        Writeln('Unknown Host ''' + pingmehost + '''');
-        Exit;
+        Writeln('Unknown Host ''' + HostName + '''');
+        ping1.destroy;  // release resources.
+        Halt;
     end;
+    DnsStatus:=True;
 
-    s:=TPinger(sender).Ping1.DnsResult;
-    writeln('Host ''' + pingmehost + ''' is ' + TPinger(sender).Ping1.DnsResult);
+    writeln('Host ''' + HostName + ''' is ' + Ping1.DnsResult);
 
-    tpinger(sender).Ping1.Address := TPinger(sender).Ping1.DnsResult;
-    TPinger(sender).Ping1.Ping;
+    Ping1.Address := Ping1.DnsResult;
+    Ping1.Ping;
 end;
 
 
@@ -108,14 +112,11 @@ begin
     Writeln(Msg);
 end;
 
-
-
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
 procedure TPinger.CancelButtonClick(Sender: TObject);
 begin
     Ping1.CancelDnsLookup;
 end;
-
 
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
 procedure TPinger.Ping1EchoRequest(Sender: TObject; Icmp: TObject);
@@ -143,36 +144,73 @@ begin
                               '. Status = ' + IntToStr(Ping1.Reply.Status));
 end;
 
+procedure TPinger.Ping;
+
+begin
+ IF DnsStatus Then
+  ping1.ping;
+end;
 
 constructor TPinger.Create(AOwner:TComponent);
 
 begin
   inherited Create(AOwner);
   ping1:=TPing.create(self);
-  Ping1.OnDnsLookupdone:=tpinger.ping1dnslookupdone;
-  Ping1.OnEchoRequest:=tpinger.ping1echorequest;
-  Ping1.OnEchoReply:=tpinger.ping1echoreply;
+  Ping1.OnDnsLookupdone:=ping1dnslookupdone;
+  Ping1.OnEchoRequest:=ping1echorequest;
+  Ping1.OnEchoReply:=ping1echoreply;
+  Hostname :='';
+  DnsStatus:=false;
+end;
+
+
+destructor TPinger.Destroy;
+
+begin
+  If Assigned(Ping1) Then Begin
+      Ping1.destroy;
+      Ping1:=NIL;
+  End;
+  Inherited Destroy;
 end;
 
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
 
-var pinger : TPinger;
-    AMessage:TMSG;
+var pinger     : TPinger;
+    AMessage   : TMSG;
+    ExitLoop   : Boolean;
+    Counter    : Integer;
+    HostToPing : String;
 
 begin
+  If ParamCount>0 Then
+     HostToPing:=ParamStr(1)
+  else begin
+      WriteLn('Usage: conping <FQHN>');
+      WriteLn;
+      Halt;
+  end;
+
   Pinger:=TPinger.Create(nil);
-  pinger.GoPingSomebody(pinger);
-   while GetMessage(@AMessage, 0, 0, 0) do begin
-    TranslateMessage(AMessage);
-    DispatchMessage(AMessage);
-   end;
-
-    writeln('waiting, pomperdom.');
-
-  readln;
-  Pinger.Destroy;
-
-
-
+  pinger.ResolveAndPing(HostToPing);
+  ExitLoop:=False;
+  Counter:=0;
+  while NOT ExitLoop do begin
+     while PeekMessage(AMessage, 0, 0,0, PM_REMOVE)=true do
+        if (AMessage.Message=WM_QUIT) or (AMessage.Message=WM_keydown) then
+           ExitLoop:=true
+        else
+           begin
+              TranslateMessage(AMessage);
+              DispatchMessage(AMessage);
+           end;
+     Sleep(50);
+     Inc(Counter);
+     if counter=21 then begin   { 20*counter=1sec}
+        Counter:=0;
+        Pinger.ping;
+      End;
+ end;
+ Pinger.Free;
 end.
 
