3a78adce93fd3d60eab0978fd80ad55f2fde8510
marco <marco@freepascal.org>
2003-09-01 07:30:29 +0000

    *  new version from Francois. Works now.
    git-svn-id: @428 -

--- trunk/contrib/ics/examples/concli1.dpr
+++ trunk/contrib/ics/examples/concli1.dpr
@@ -4,10 +4,10 @@ Author:       François PIETTE
 Description:  ConCli shows how to use TWSocket in a console mode application
               (for Delphi 2, Delphi 3 or Delphi 4).
 Creation:     Nov 20, 1997
-Version:      1.01
-EMail:        http://www.overbyte.be        http://www.rtfm.be/fpiette
-              francois.piette@overbyte.be   francois.piette@rtfm.be
-                                            francois.piette@pophost.eunet.be
+Version:      1.02
+EMail:        francois.piette@overbyte.be  http://www.overbyte.be
+              francois.piette@rtfm.be      http://www.rtfm.be/fpiette
+              francois.piette@pophost.eunet.be
 Support:      Use the mailing list twsocket@elists.org
               Follow "support" link at http://www.overbyte.be for subscription.
 Legal issues: Copyright (C) 1997-2002 by François PIETTE
@@ -35,9 +35,16 @@ Legal issues: Copyright (C) 1997-2002 by François PIETTE
               3. This notice may not be removed or altered from any source
                  distribution.
 
+              4. You must register this software by sending a picture postcard
+                 to the author. Use a nice stamp and mention your name, street
+                 address, EMail address and any comment you like to say.
+
+
 Updates:
 Dec 05, 1998 V1.01 Don't use TWait control anymore
-}
+Aug 31, 2003 V1.02 Completely rewritten sample.
+
+
 {* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
 {$IFDEF VER80}
     Bomb('Sorry, Delphi 1 does not support console mode programs');
@@ -48,33 +55,143 @@ program ConCli1;
 {$i icsdef.inc}
 
 uses
-    {$Ifndef NOFORMS} Forms, {$endif}  {$ifdef FPC} custapp,{$endif} WSocket;
+    SysUtils,
+{$IFNDEF NOFORMS}
+    { TWSocket component is able to work with or without the Forms unit.    }
+    { In a console mode application, the Forms unit is not very interesting }
+    { because we don't have a form to display. Avoiding the forms unit use, }
+    { our application suddenly become a lot smaller !                       }
+    Forms,
+{$ENDIF}
+    WSocket;
+
+const
+    ConCli1Version     = 102;
+    CopyRight : String = 'ConCli1 (c) 1997-2003 Francois Piette  V1.02 ';
+
+type
+    { TWSocket is event driven. Since all event handlers have to be a       }
+    { "procedure of object", we need to define an object to be able to      }
+    { definie event handlers. Anyway, it is handy to add our data.          }
+    { We define our own object because Delphi runtime doesn't allow to      }
+    { derive from or redefine TApplication object.                          }
+    TConCliApp = class
+    public
+        WSocket1 : TWSocket;
+        Buffer   : String;
+        DoneFlag : Boolean;
+        procedure WSocketDataAvailable(Sender : TObject; Error : Word);
+        procedure WSocketSessionConnected(Sender : TObject; Error : Word);
+        procedure WSocketSessionClosed(Sender : TObject; Error : Word);
+        procedure Run;
+    end;
 
 var
-    WSocket1 : TWSocket;
-    Buffer   : String;
-begin
-    WSocket1          := TWsocket.Create(nil);
-    WSocket1.Proto    := 'tcp';
-    WSocket1.Addr     := 'localhost';
-    WSocket1.Port     := 'telnet';
-    WSocket1.Connect;
-    { Connect is asynchronous (non-blocking). We will wait while the }
-    { session is connecting or application terminated.               }
-    while WSocket1.State in [wsConnecting] do begin
-        Application.ProcessMessages;
-        if Application.Terminated then
-            break;
-    end;
-    if WSocket1.State = wsConnected then begin
-        WSocket1.ReadLine(15, Buffer);
-        Writeln('Server banner is: ' + Buffer);
-        WSocket1.Close;
-    end
-    else
-        Writeln('Connection failed');
-    WSocket1.Destroy;
+    ConApp : TConCliApp;
 
+
+
+{* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
+{ The Run procedure is where the application ocde is.                       }
+procedure TConCliApp.Run;
+begin
+    { TWSocket is event driven. We will have a message loop to trigger all  }
+    { event. We have to stop the loop when something happend. The DoneFlag  }
+    { is used for that purpose.                                             }
+    DoneFlag                 := FALSE;
+    { Create TWSocket used by this application.                             }
+    WSocket1                 := TWsocket.Create(nil);
+    { Initialize paramters for the connection                               }
+    WSocket1.Proto           := 'tcp';
+    WSocket1.Addr            := 'localhost';
+    WSocket1.Port            := 'telnet';
+    { Initialize all required event handlers                                }
+    WSocket1.OnSessionConnected := WSocketSessionConnected;
+    WSocket1.OnSessionClosed    := WSocketSessionClosed;
+    WSocket1.OnDataAvailable    := WSocketDataAvailable;
+    { Request connection. This is non blocking, event driven.               }
+    { Connect will retrun immediately while connection take place in the    }
+    { background without blocking the program. When the connection is       }
+    { established, we will get the OnSessionConnected event. When it is     }
+    { we will get the OnSessionClosed event. When we receive data, the      }
+    { OnDataAvailable event is triggered.                                   }
+    try
+        WriteLn('Connecting to ' + WSocket1.Addr + '/' + WSocket1.Port);
+        WSocket1.Connect;
+        { Now we enter the message loop. Everything else until the          }
+        { connection is closed occur in the event handlers.                 }
+{$IFDEF NOFORMS}
+        while not DoneFlag do
+            WSocket1.ProcessMessages;
+{$ELSE}
+        while not DoneFlag do begin
+            Application.ProcessMessages;
+            if Application.Terminated then
+                break;
+        end;
+{$ENDIF}
+        { Here the application is almost done. The session is closed.       }
+        WriteLn('Done.');
+    except
+        on E:Exception do
+            WriteLn('An error occured. ', E.ClassName + ': ' + E.Message);
+    end;
+
+    WSocket1.Destroy;
     Writeln('Hit enter...');
     Readln;
+end;
+
+{* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
+{ This event handler is called by the TWSocket when some data has been      }
+{ received by the lower level.                                              }
+procedure TConCliApp.WSocketDataAvailable(Sender : TObject; Error : Word);
+begin
+    { Set the flag which will end the message loop }
+    DoneFlag := TRUE;
+    { Receive data from server }
+    Buffer   := WSocket1.ReceiveStr;
+    Writeln('Server banner is: ' + Buffer);
+    { This very simple application receive only one line... }
+    { So we just close the connection gracefuly.            }
+    WSocket1.Close;
+end;
+
+
+{* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
+{ This event handler is called by TWSocket when the connection is ready     }
+procedure TConCliApp.WSocketSessionConnected(Sender : TObject; Error : Word);
+begin
+    if Error = 0 then
+        WriteLn('Connected.')
+    else begin
+        WriteLn('Unable to connect. Error #' + IntToStr(Error));
+        DoneFlag := TRUE;
+    end;
+end;
+
+
+{* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
+{ This event handler is called by TWSocket when the connection is closed    }
+procedure TConCliApp.WSocketSessionClosed(Sender : TObject; Error : Word);
+begin
+    { Set the flag which will end the message loop }
+    DoneFlag := TRUE;
+    if Error = 0 then
+        WriteLn('Session closed')
+    else
+        WriteLn('Session closed with error #' + IntToStr(Error))
+end;
+
+
+{* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *}
+{ This is the main entry point for the application                          }
+begin
+    WriteLn(CopyRight);
+    ConApp := TConCliApp.Create;  { Create our application object           }
+    try                           { Trap any exception                      }
+        ConApp.Run;               { Run the application code                }
+    finally                       { Be sure to cleanup even with excepions  }
+        ConApp.Destroy;           { Destroy the application object          }
+    end;
 end.
