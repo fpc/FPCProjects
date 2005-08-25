eaf850699298536220e8c928b0aa348dac73095e
Almindor <almindor@gmail.com>
2005-08-25 17:59:19 +0000

    Added some date utils
    git-svn-id: @575 -

--- fpcircbot/common/stringutils.pp
+++ fpcircbot/common/stringutils.pp
@@ -25,6 +25,12 @@ function return_Nick_sanitize (const Nick : String) : String;
 function return_datetimestamp_sanitize (const DateTimeStamp : String) : String;
 //This function remove any illeagl chars for DateTimeStamp
 
+function SQLStrToDateTimeStr(const SQLStr: string): string;
+//This function converts datetime string returned by SQL(timestamp.asstring) into a TDateTime compatible string
+
+function CreateDiffStr(const DT1, DT2: TDateTime): string;
+//This function creates a string diff for TDateTime diff with 'years months' etc. It only fills parts which are not 0.
+
 implementation
 
 function RemoveOtherChars (const S : String; AllowedChars : TSysCharSet) : String; {$IFDEF USEINLINE} inline;{$ENDIF}
@@ -89,8 +95,8 @@ begin
  if (Length (Result) > 0) then
    begin
 		 Result := StringReplace (Result, #0, '', [rfReplaceAll]);  //Remove Null terminator chars
-	   Result := StringReplace (Result, #32, '', [rfReplaceAll]); //Remove spaces.
-     Result := StringReplace (Result, #7, '', [rfReplaceAll]);  //Remove BELL
+                 Result := StringReplace (Result, #32, '', [rfReplaceAll]); //Remove spaces.
+                 Result := StringReplace (Result, #7, '', [rfReplaceAll]);  //Remove BELL
 		 Result := StringReplace (Result, #13, '', [rfReplaceAll]); //Remove CR
 		 Result := StringReplace (Result, #10, '',  [rfReplaceAll]); //Remove LF
 		 Result := StringReplace (Result, ',', '', [rfReplaceAll]); //Remove Comma
@@ -119,6 +125,57 @@ begin
   Result := DateTimeStamp;
 	if Length (Result) > 0 then
 	  Result := RemoveOtherChars (Result, ['0'..'9', '-', #32, ':']);
+end;
+
+function SQLStrToDateTimeStr(const SQLStr: string): string;
+var
+  s: string;
+begin
+  Result:='';
+  s:=SQLStr;
+  System.Delete(s, Length(s) - 5, 5);
+  Result:=Copy(s, 9, 2) + '-' + Copy(s, 6, 2) + '-' + Copy(s, 3, 2) + Copy(s, 11, Length(s));
+  System.Delete(Result, Length(Result) - 3, 3);
+  if Result[4] = '0' then System.Delete(Result, 4, 1);
+end;
+
+function CreateDiffStr(const DT1, DT2: TDateTime): string;
+
+  function MultiString(const x: Word): string;
+  begin
+    if x > 1 then Result:='s' else Result:='';
+  end;
+
+var
+  Year, Day, Hour, Minute: Integer;
+  
+  DiffTIme: Integer;
+begin
+  Result:='';
+  Writeln('Doing diff between: ', DateTimeToStr(DT1), ' | ', DateTimeToStr(DT2));
+  DiffTime  := Abs(DateTimeToTimeStamp(DT1).Time - DateTimeToTimeStamp(DT2).Time);
+
+  Day       := Abs(DateTimeToTimeStamp(DT1).Date - DateTimeToTimeStamp(DT2).Date);
+
+  {
+    imagine this diff: DT1 = (01, 01, 0001 23:59) DT2 = (02, 01, 0001 00:01)
+    The result would be DTResult = (1 day 23 hours and 58 minutes)
+    So what we need to get 2 minute here is (and the rule should work generaly):
+    if day is decreasable by 1 do so, and if so then subtract time from 24 hours.
+  }
+  if Day > 0 then begin
+    Dec(Day);
+    DiffTime := Abs((24 * 1000 * 60 * 60) - DiffTime);
+  end;
+
+  Year      := Day div 356; // let's assume noone is gone long enough for leap year to make effect
+  Hour      := ((DiffTime div 1000) div 60) div 60;
+  Minute    := ((DiffTime div 1000) div 60) mod 60;
+
+  if Year > 0 then Result:=Result + IntToStr(Year) + ' year' + MultiString(Year) + ' ';
+  if Day > 0 then Result:=Result + IntToStr(Day) + ' day' + MultiString(Day) + ' ';;
+  if Hour > 0 then Result:=Result + IntToStr(Hour) + ' hour' + MultiString(Hour) + ' ';
+  if Minute > 0 then Result:=Result + IntToStr(Minute) + ' minute' + MultiString(Minute) + ' ';
 end;
 
 end.
