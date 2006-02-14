e9235d756fd056d9a76701659fbef47e6744958c
Almindor <almindor@gmail.com>
2006-02-14 22:02:28 +0000

    Initial import of lNetPackage source tree
    git-svn-id: @644 -

---
+++ lnet/lnetpackage/lnetcomponents.pas
@@ -0,0 +1,112 @@
+{ lNetComponents v0.3
+
+  CopyRight (C) 2004-2006 Ales Katona
+
+  This library is Free software; you can rediStribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is diStributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; withOut even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+  for more details.
+
+  You should have received a Copy of the GNU Library General Public License
+  along with This library; if not, Write to the Free Software Foundation,
+  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+  This license has been modified. See File LICENSE.ADDON for more inFormation.
+  Should you find these sources without a LICENSE File, please contact
+  me at ales@chello.sk
+}
+
+
+unit lNetComponents;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  LCLNet, lNet, lFTP;
+
+type
+  TLSocket = lNet.TLSocket;
+  TLConnection = lNet.TLConnection;
+  TLNetComponent = TLConnection;
+  TLTcp = lNet.TLTcp;
+  TLUdp = lNet.TLUdp;
+  TLFtpClient = lFTP.TLFTPClient;
+
+  TLErrorProc = procedure(const msg: string; aSocket: TLSocket) of object;
+  TLProc = procedure(aSocket: TLSocket) of object;
+
+  TLFTPClientProgressCallback = procedure (Sender: TLFTPClient; const Bytes: Integer) of object;
+  TLFTPClientCallback = procedure (Sender: TLFTPClient) of object;
+
+  { TLTCPComponent }
+
+  TLTCPComponent = class(TLBaseTcp)
+  public
+    constructor Create(aOwner: TComponent); override;
+  published
+    property OnReceive;
+    property OnError;
+    property OnDisconnect;
+    property OnConnect;
+    property OnAccept;
+  end;
+  
+  { TLUDPComponent }
+
+  TLUDPComponent = class(TLBaseUdp)
+  public
+    constructor Create(aOwner: TComponent); override;
+  published
+    property OnReceive;
+    property OnError;
+  end;
+
+  { TLFTPClientComponent }
+
+  TLFTPClientComponent = class(TLBaseFTPClient)
+  public
+    constructor Create(aOwner: TComponent); override;
+  published
+    property OnConnect;
+    property OnSent;
+    property OnReceive;
+    property OnControl;
+    property OnError;
+    property PipeLine;
+    property StartPort;
+    property UsePORT;
+  end;
+
+implementation
+
+
+{ TLTCPComponent }
+
+constructor TLTCPComponent.Create(aOwner: TComponent);
+begin
+  inherited Create(aOwner, TLCLSocket);
+end;
+
+{ TLUDPComponent }
+
+constructor TLUDPComponent.Create(aOwner: TComponent);
+begin
+  inherited Create(aOwner, TLCLSocket);
+end;
+
+{ TLFTPClientComponent }
+
+constructor TLFTPClientComponent.Create(aOwner: TComponent);
+begin
+  inherited Create(aOwner, TLCLSocket);
+end;
+
+end.
