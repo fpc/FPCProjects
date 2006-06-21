5bb1a2e70c2b477974eb3729ac799862ddbeb5c2
Almindor <almindor@gmail.com>
2006-06-21 19:51:56 +0000

    * adds smtp example for visual package
    git-svn-id: @807 -

---
+++ lnet/trunk/examples/visual/smtp/main.pas
@@ -0,0 +1,65 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Buttons, lNetComponents;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    ConnectButton: TButton;
+    ConnectionBox: TGroupBox;
+    SMTP: TLSMTPClientComponent;
+    PortEdit: TEdit;
+    PortLabel: TLabel;
+    ServerLabel: TLabel;
+    ServerEdit: TEdit;
+    procedure ConnectButtonClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure SMTPConnect(Sender: TLSMTPClient);
+    procedure SMTPError(const msg: string; aSocket: TLSocket);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{ TMainForm }
+
+procedure TMainForm.ConnectButtonClick(Sender: TObject);
+begin
+  SMTP.Connect(ServerEdit.Text, Word(StrToInt(PortEdit.Text)));
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  SMTP.OnError:=@SMTPError;
+end;
+
+procedure TMainForm.SMTPConnect(Sender: TLSMTPClient);
+begin
+  ShowMessage('connected');
+end;
+
+procedure TMainForm.SMTPError(const msg: string;
+  aSocket: TLSocket);
+begin
+  ShowMessage(msg);
+end;
+
+initialization
+  {$I main.lrs}
+
+end.
+
