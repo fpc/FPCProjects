c3bc6af15ea4f4cbe2fddff98fccf2646d77004a
crossbuilder <crossbuilder@freepascal.org>
2006-11-23 14:02:42 +0000

    + fixed glspacetext
    git-svn-id: @1114 -

--- glscene/trunk/Source/glspacetext.pas
+++ glscene/trunk/Source/glspacetext.pas
@@ -18,6 +18,7 @@
       - added automatical generated History from CVS
 
 	<b>History : </b><font size=-1><ul>
+      <li>16/09/06 - NC - TGLVirtualHandle update (thx Lionel Reynaud)
       <li>03/06/02 - EG - VirtualHandle notification fix (Sören Mühlbauer)
       <li>07/03/02 - EG - GetFontBase fix (Sören Mühlbauer)
       <li>30/01/02 - EG - Text Alignment (Sören Mühlbauer),
@@ -167,8 +168,8 @@ type
 	   protected
 			{ Protected Declarations }
          procedure NotifyClients(Clients:TList);
-         procedure VirtualHandleAlloc(sender : TGLVirtualHandle; var handle : Integer);
-         procedure VirtualHandleDestroy(sender : TGLVirtualHandle; var handle : Integer);
+         procedure VirtualHandleAlloc(sender : TGLVirtualHandle; var handle : Cardinal);
+         procedure VirtualHandleDestroy(sender : TGLVirtualHandle; var handle : Cardinal);
 
 	   public
 			{ Public Declarations }
@@ -392,12 +393,11 @@ begin
       glPushAttrib(GL_POLYGON_BIT);
       case FCharacterRange of
         stcrAlphaNum : glListBase(FTextFontEntry^.FVirtualHandle.Handle - 32);
-        stcrNumbers :  glListBase(FTextFontEntry^.FVirtualHandle.Handle - Integer('0'));
+        stcrNumbers :  glListBase(FTextFontEntry^.FVirtualHandle.Handle - Cardinal('0'));
       else
         glListBase(FTextFontEntry^.FVirtualHandle.Handle);
       end;
-//      glCallLists(Length(FText), GL_UNSIGNED_BYTE, PChar(FText));
-      glCallLists(Length(FText), GL_UNSIGNED_BYTE, @ShortString(FText));
+      glCallLists(Length(FText), GL_UNSIGNED_BYTE, PChar(FText));
       glPopAttrib;
 
       glPopMatrix;
@@ -578,14 +578,14 @@ end;
 
 // VirtualHandleAlloc
 //
-procedure TFontManager.VirtualHandleAlloc(sender : TGLVirtualHandle; var handle : Integer);
+procedure TFontManager.VirtualHandleAlloc(sender : TGLVirtualHandle; var handle : Cardinal);
 begin
    handle:=FCurrentBase;
 end;
 
 // VirtualHandleDestroy
 //
-procedure TFontManager.VirtualHandleDestroy(sender : TGLVirtualHandle; var handle : Integer);
+procedure TFontManager.VirtualHandleDestroy(sender : TGLVirtualHandle; var handle : Cardinal);
 begin
    if handle<>0 then
       glDeleteLists(handle, sender.Tag);
