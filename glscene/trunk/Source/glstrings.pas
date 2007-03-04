d6191997615245ee314c7bd79686fb2488b75f9f
crossbuilder <crossbuilder@freepascal.org>
2007-03-04 20:43:27 +0000

    * updated GLStrings to cvs r1.7
    git-svn-id: @1330 -

--- glscene/trunk/Source/glstrings.pas
+++ glscene/trunk/Source/glstrings.pas
@@ -1,6 +1,9 @@
-{: glvectorfileobjects<p>
+//
+// This unit is part of the GLScene Project, http://glscene.org
+//
+{: GLStrings<p>
 
-	Vector File related objects for GLScene<p>
+	String constants that are used in many GLScene units<p>
 
       $Log: glstrings.pas,v $
       Revision 1.1  2006/01/10 20:50:46  z0m3ie
@@ -19,15 +22,25 @@
       - added automatical generated History from CVS
 
 	<b>History :</b><font size=-1><ul>
+      23/02/07 - DaStr - Added glsDot, glsUnsupportedType, glsUncompatibleTypes,
+                         glsUnknownType, glsShaderNeedsAtLeastOneLightSource(Ex),
+                         glsCadencerNotDefined(Ex), glsSceneViewerNotDefined
+      16/02/07 - DaStr - Added glsOCProxyObjects, glsError, glsErrorEx,
+                         glsMatLibNotDefined, glsMaterialNotFoundInMatlib(Ex)
       26/08/02 - EG - Added missing header, added glsUnknownExtension
 	</ul></font>
 }
-unit glstrings;
+unit GLStrings;
 
 interface
 
-resourcestring
+const
+  // General
+  glsDot = '.';
+  glsError   = 'Error!';
+  glsErrorEx = 'Error: ';
 
+resourcestring
   // SceneViewer
   glsNoRenderingContext = 'Could not create a rendering context';
   glsWrongVersion       = 'Need at least OpenGL version 1.1';
@@ -36,6 +49,15 @@ resourcestring
   glsWrongBitmapCanvas  = 'Couldn''t create a rendering context for the given bitmap';
   glsWrongPrinter       = 'Couldn''t render to printer';
   glsAlreadyRendering   = 'Already rendering';
+  glsSceneViewerNotDefined = glsErrorEx + 'SceneViewer not defined!';
+
+  // GLCadencer
+  glsCadencerNotDefined   = glsErrorEx + 'Cadencer not defined!';
+  glsCadencerNotDefinedEx = glsErrorEx + 'Cadencer not defined for  the ''%s'' component';
+
+  // Shaders
+  glsShaderNeedsAtLeastOneLightSource   = glsErrorEx + 'This shader needs at least one LightSource!';
+  glsShaderNeedsAtLeastOneLightSourceEx = glsErrorEx + 'Shader ''%s'' needs at least one LightSource!';
 
   // GLTree
   glsSceneRoot  = 'Scene root';
@@ -46,6 +68,11 @@ resourcestring
   // GLTexture
   glsImageInvalid = 'Could not load texture, image is invalid';
   glsNoNewTexture = 'Could not get new texture name';
+
+  // GLMaterials
+  glsMatLibNotDefined = glsErrorEx + 'Material Library not defined!';
+  glsMaterialNotFoundInMatlib = glsErrorEx + 'Material not found in current Material Library!';
+  glsMaterialNotFoundInMatlibEx = glsErrorEx + 'Material "%s" not found in current Material Library!';
 
   // GLObjects
   glsSphereTopBottom = 'The top angle must be higher than the bottom angle';
@@ -61,6 +88,10 @@ resourcestring
   glsUnknownExtension = 'Unknown file extension (%s), maybe you forgot to add the support '
                        +'unit to your uses? (%s?)' ;
 
+  glsUncompatibleTypes = glsErrorEx + 'uncompatible types!';
+  glsUnknownType       = glsErrorEx + 'unknown type!';
+  glsUnsupportedType   = glsErrorEx + 'unsupported type!';
+
   // object categories
   glsOCBasicGeometry = 'Basic geometry';
   glsOCAdvancedGeometry = 'Advanced geometry';
@@ -72,6 +103,7 @@ resourcestring
   glsOCDoodad = 'Doodad objects';
   glsOCHUDObjects = 'HUD objects';
   glsOCGuiObjects = 'GUI objects';
+  glsOCProxyObjects = 'Proxy objects';
 
 implementation
 
