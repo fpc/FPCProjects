ba883f5d51c6045b282489aec4535c63ab196458
Almindor <almindor@gmail.com>
2007-03-15 20:21:39 +0000

    * alpha color-coding
    git-svn-id: @1445 -

--- fpcircbot/cgi/cgifpcbot.pas
+++ fpcircbot/cgi/cgifpcbot.pas
@@ -18,6 +18,18 @@ const
   HTML_ORANGE = '#ad10ad';
   HTML_GREEN  = '#109d10';
   HTML_BLUE   = '#10109d';
+  
+  CTRL_C = #3;
+
+  AllowedChars = [CTRL_C, #48..#75, ','];
+
+  ColorCodeAr: array[#48..#75] of string = ('FFFFFF', '000000', 'FF0000', 'FF8000',
+                                            'FFFF00', '80FF00', '00FF00', '00FF80',
+                                            '00FFFF', '0080FF', '0000FF', '8000FF',
+                                            'FF00FF', 'FF0080', 'C0C0C0', '404040',
+                                            '800000', '804000', '808000', '408000',
+                                            '008000', '008040', '008080', '004080',
+                                            '000080', '400080', '800080', '800040');
 
 var
   LogConnection   : TPQConnection;
@@ -48,14 +60,14 @@ begin
   with LogQuery do begin
     DataBase := LogConnection;
     transaction := LogTransaction;
-    ReadOnly:=True;
+    ReadOnly := True;
   end;
 
   ChanQuery := tsqlquery.Create(nil);
   with ChanQuery do begin
     DataBase := LogConnection;
     transaction := LogTransaction;
-    ReadOnly:=True;
+    ReadOnly := True;
   end;
 end;
 
@@ -70,17 +82,17 @@ end;
 
 procedure InitCommon;
 begin
-  Channel:='';
-  Sender:='';
-  Count:='50';
-  Msg:='';
-  ChanList:=TStringList.Create;
-  LineList:=TStringList.Create;
+  Channel := '';
+  Sender := '';
+  Count := '50';
+  Msg := '';
+  ChanList := TStringList.Create;
+  LineList := TStringList.Create;
 end;
 
 procedure Init;
 begin
-  ShortDateFormat:='dd"-"mmm"-"yy';
+  ShortDateFormat := 'dd"-"mmm"-"yy';
   InitCommon;
   InitDB;
 end;
@@ -102,30 +114,30 @@ procedure GetWebVars;
   procedure TryGetWebVar(var LocalVar: string; const VarName, DefValue: string);
   begin
     if Length(GetCGIVar(VarName)) > 0 then begin
-      LocalVar:=GetCGIVar(VarName);
+      LocalVar := GetCGIVar(VarName);
       if VarName = 'channel' then
-        LocalVar:='#' + LocalVar;
+        LocalVar := '#' + LocalVar;
       SetWebVar(VarName, LocalVar);
     end else if Length(GetWebVar(VarName)) > 0 then try
-      LocalVar:=GetWebVar(VarName);
+      LocalVar := GetWebVar(VarName);
       if Length(LocalVar) = 0 then
-        LocalVar:=DefValue;
+        LocalVar := DefValue;
       if VarName = 'channel' then
-        LocalVar:='#' + LocalVar;
+        LocalVar := '#' + LocalVar;
     except
-      LocalVar:=DefValue;
+      LocalVar := DefValue;
     end else begin
       SetWebVar(VarName, DefValue);
-      LocalVar:=DefValue;
+      LocalVar := DefValue;
     end;
   end;
 
 var
   DefChan: string;
 begin
-  DefChan:='';
+  DefChan := '';
   if ChanList.Count > 0 then
-    DefChan:='#' + ChanList[0];
+    DefChan := '#' + ChanList[0];
 
   TryGetWebVar(Channel, 'channel', DefChan);
 	
@@ -172,17 +184,17 @@ var
   s: string;
   i: Integer;
 begin
-  Result:='';
+  Result := '';
   if ChanList.Count > 0 then
-    for i:=0 to ChanList.Count-1 do begin
-      s:=ChanList[i];
+    for i := 0 to ChanList.Count-1 do begin
+      s := ChanList[i];
       if Length(s) > 0 then System.Delete(s, 1, 1); // Delete #
-      Result:=Result + '<option value="' + s + '"';
+      Result := Result + '<option value="' + s + '"';
       if '#' + s <> Channel then
-        Result:=Result + '>'
+        Result := Result + '>'
       else
-        Result:=Result + ' selected>';
-      Result:=Result + '#' + s + '</option>';
+        Result := Result + ' selected>';
+      Result := Result + '#' + s + '</option>';
     end;
 end;
 
@@ -196,31 +208,31 @@ var
     i: Integer;
   begin
     if x > 0 then begin
-      for i:=x + 1 to Length(s) do
+      for i := x + 1 to Length(s) do
         if s[i] = ' ' then Break;
-      tmp:=Trim(Copy(s, x + 1, i - x));
-      s:=StringReplace(s, ' ' + tmp, ' <a href="' + aps + tmp + '" target="new">' + tmp + '</a>', [rfReplaceAll]);
+      tmp := Trim(Copy(s, x + 1, i - x));
+      s := StringReplace(s, ' ' + tmp, ' <a href="' + aps + tmp + '" target="new">' + tmp + '</a>', [rfReplaceAll]);
     end;
   end;
 
 begin
-  s:=' ' + InStr;
+  s := ' ' + InStr;
 
-  n:=Pos(' http://', LowerCase(s));
+  n := Pos(' http://', LowerCase(s));
   Convert(n, '');
 
-  n:=Pos(' https://', LowerCase(s));
+  n := Pos(' https://', LowerCase(s));
   Convert(n, '');
 
-  n:=Pos(' www.', LowerCase(s));
+  n := Pos(' www.', LowerCase(s));
   Convert(n, 'http://');
 
-  s:=Trim(s);
+  s := Trim(s);
 
   if Pos(' www.', s) + Pos(' http://', s) + Pos(' https://', s) > 0 then
-    Result:=HighlightHyperlinks(s)
+    Result := HighlightHyperlinks(s)
   else
-    Result:=s;
+    Result := s;
 end;
 
 function ValidRequest: Boolean;
@@ -228,7 +240,64 @@ function ValidRequest: Boolean;
   d1, d2: TDateTime;
   i: Integer;}
 begin
-  Result:=True;
+  Result := True;
+end;
+
+function ColorCodeToHtml(var s: string; Tmp: string; var i: Integer): Boolean;
+const
+  Crap = [CTRL_C, '0'];
+begin
+  Delete(s, i - Length(Tmp), Length(Tmp));
+  Dec(i, Length(Tmp));
+  Result := False;
+
+  if Tmp[1] = CTRL_C then begin
+    while (Length(Tmp) > 0) and (Tmp[1] in Crap) do
+      Delete(Tmp, 1, 1);
+
+    if Pos(',', Tmp) > 0 then
+      Delete(Tmp, Pos(',', Tmp), Length(Tmp)); // remove background color crap
+
+    if Length(Tmp) = 0 then begin
+      Insert('</font>', s, i);
+      Inc(i, Length('</font>'));
+      Exit;
+    end;
+
+    if Length(Tmp) <> 1 then
+      raise Exception.Create('WTFF? ' + Tmp);
+
+    Insert('<font color="#' + ColorCodeAr[Tmp[1]] + '">', s, i);
+    Inc(i, Length('<font color="#' + ColorCodeAr[Tmp[1]] + '">'));
+    Result := True;
+  end;
+end;
+
+function DecodeColor(s: string): string;
+var
+  i: Integer;
+  Tmp: string;
+  InFont: Boolean = False;
+begin
+  Result := '';
+  i := 1;
+  Tmp := '';
+
+  while i <= Length(s) do begin
+    if s[i] in AllowedChars then
+      Tmp := Tmp + s[i]
+    else if Length(Tmp) > 0 then begin
+      InFont := ColorCodeToHtml(s, Tmp, i);
+      Tmp := '';
+    end;
+
+    Inc(i);
+  end;
+
+  if InFont then
+    s := s + '</font>';
+
+  Result := s;
 end;
 
 var
@@ -236,14 +305,14 @@ var
   Flip: Boolean;
 begin
   Init;
-  LN:='';
-  s:='';
+  LN := '';
+  s := '';
 
   FillChannels;
   GetWebVars;
   
   if Channel = '#' then
-    if ChanList.Count > 0 then Channel:=ChanList[0];
+    if ChanList.Count > 0 then Channel := ChanList[0];
 
   WebTemplateOut('html' + PathDelim + 'header1.html', False);
 
@@ -253,17 +322,17 @@ begin
 
   WebWriteln('<table class="body_style">');
 
-  if Length(Sender) > 0 then s:=s + ' and UPPER(sender)=' + UpperCase(AnsiQuotedStr(SQLEscape(Sender), #39));
-  if Length(Msg) > 0 then s:=s + ' and msg like ''%' + SQLEscape(Msg) + '%''';
-  if Length(FromDate) > 0 then s:=s + ' and CAST (logtime as date) >= ' + AnsiQuotedStr(SQLEscape (FromDate), #39) ;
-  if Length(ToDate) > 0 then s:=s + ' and CAST (logtime as date) <= ' + AnsiQuotedStr(SQLEscape (ToDate), #39) ;
-  if Length(FromTime) > 0 then s:=s + ' and CAST (logtime as time) >= ' + AnsiQuotedStr(SQLEscape (FromTime), #39) ;
-  if Length(ToTime) > 0 then s:=s + ' and CAST (logtime as time) <= ' + AnsiQuotedStr(SQLEscape (ToTime), #39) ;
+  if Length(Sender) > 0 then s := s + ' and UPPER(sender)=' + UpperCase(AnsiQuotedStr(SQLEscape(Sender), #39));
+  if Length(Msg) > 0 then s := s + ' and msg like ''%' + SQLEscape(Msg) + '%''';
+  if Length(FromDate) > 0 then s := s + ' and CAST (logtime as date) >= ' + AnsiQuotedStr(SQLEscape (FromDate), #39) ;
+  if Length(ToDate) > 0 then s := s + ' and CAST (logtime as date) <= ' + AnsiQuotedStr(SQLEscape (ToDate), #39) ;
+  if Length(FromTime) > 0 then s := s + ' and CAST (logtime as time) >= ' + AnsiQuotedStr(SQLEscape (FromTime), #39) ;
+  if Length(ToTime) > 0 then s := s + ' and CAST (logtime as time) <= ' + AnsiQuotedStr(SQLEscape (ToTime), #39) ;
 
   if  (Count = '9999')
   and (Length(FromDate) = 0)
   and (Length(ToDate) = 0) then
-    s:=s + ' and CAST (logtime as date) >= ' + AnsiQuotedStr(SQLEscape (DateTimeToStr(Now)), #39) ;
+    s := s + ' and CAST (logtime as date) >= ' + AnsiQuotedStr(SQLEscape (DateTimeToStr(Now)), #39) ;
 
   if ValidRequest then begin
     with LogQuery do try
@@ -272,28 +341,30 @@ begin
               'where (reciever=' + AnsiQuotedStr(SQLEscape(Channel), #39) + s + ') ' +
               'order by logtime desc limit ' + SQLEscape(Count) + ') as selection order by logtime');
       open;
-      Flip:=False;
-      LN:=FilterHtml(fieldbyname('sender').asstring);
+      Flip := False;
+      LN := FilterHtml(fieldbyname('sender').asstring);
       while not eof do begin
-        s:=FilterHtml(fieldbyname('sender').asstring);
-        smsg:=FilterHtml(fieldbyname('msg').asstring);
-        if s <> LN then Flip:=not Flip;
+        s := FilterHtml(fieldbyname('sender').asstring);
+        smsg := FilterHtml(fieldbyname('msg').asstring);
+        if s <> LN then Flip := not Flip;
 
-        TheColor:='';
+        TheColor := '';
         if Pos(s, smsg) = 1 then begin
-          if Pos('quits(', smsg) = Length(s) + 2 then TheColor:=';color:'+ HTML_RED;
-          if Pos('leaves ' + Channel, smsg) = Length(s) + 2 then TheColor:=';color:'+ HTML_RED;
-          if Pos('joins ' + Channel, smsg) = Length(s) + 2 then TheColor:=';color:' + HTML_GREEN;
-          if Pos(s + ' is now known as ', smsg) = 1 then TheColor:=';color:' + HTML_ORANGE;
-        end else if s = '*' then TheColor:=';color:' + HTML_BLUE;
-
+          if Pos('quits(', smsg) = Length(s) + 2 then TheColor := ';color:'+ HTML_RED;
+          if Pos('leaves ' + Channel, smsg) = Length(s) + 2 then TheColor := ';color:'+ HTML_RED;
+          if Pos('joins ' + Channel, smsg) = Length(s) + 2 then TheColor := ';color:' + HTML_GREEN;
+          if Pos(s + ' is now known as ', smsg) = 1 then TheColor := ';color:' + HTML_ORANGE;
+        end else if s = '*' then TheColor := ';color:' + HTML_BLUE;
+        
+        smsg := DecodeColor(smsg);
+        
         WebWriteln('<tr style="background-color:' + ColorAr[Flip] + TheColor + '">' +
                    '<td nowrap width="1%">[' +
                     fieldbyname('logtime').asstring +
                    ']' + '</td><td nowrap width="1%">' + s +
                    ': </td><td>' + HighlightHyperLinks(smsg) + '</td></tr>');
 
-        LN:=s;
+        LN := s;
         Next;
       end;
       close;
