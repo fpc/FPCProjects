9e566d07b996529cfd1118a144aa51dac34115eb
Almindor <almindor@gmail.com>
2007-04-04 18:11:35 +0000

    * adds comments and cleans code a bit for SMTP
    git-svn-id: @1605 -

--- lnet/trunk/examples/console/lsmtp/lsmtpclient.pp
+++ lnet/trunk/examples/console/lsmtp/lsmtpclient.pp
@@ -13,10 +13,16 @@ type
 
   TLSMTPClientTest = class
    private
-    FSMTP: TLSMTPClient;
-    FQuit: Boolean;
+    FSMTP: TLSMTPClient; // this is THE smtp connection
+    FQuit: Boolean;  // helper for main loop
     function GetAnswer(const s: string): string;
     procedure PrintUsage(const Msg: string);
+   {  these events are used to see what happens on the SMTP connection. They are used via "CallAction".
+      OnReceive will get fired whenever new data is received from the SMTP server.
+      OnConnect will get fired when connecting to server ended with success.
+      OnDisconnect will get fired when the other side closed connection gracefully.
+      OnError will get called when any kind of net error occurs on the connection.
+   }
     procedure OnReceive(aSocket: TLSocket);
     procedure OnConnect(aSocket: TLSocket);
     procedure OnDisconnect(aSocket: TLSocket);
@@ -24,7 +30,7 @@ type
    public
     constructor Create;
     destructor Destroy; override;
-    procedure Run;
+    procedure Run; // main loop 
   end;
 
 { TLSMTPClientTest }
@@ -69,31 +75,31 @@ procedure TLSMTPClientTest.OnReceive(aSocket: TLSocket);
 var
   s: string;
 begin
-  FSMTP.GetMessage(s);
-  Write(s);
+  if FSMTP.GetMessage(s) > 0 then // if we actually received something from SMTP server, write it for the user
+    Write(s);
 end;
 
 procedure TLSMTPClientTest.OnConnect(aSocket: TLSocket);
 begin
-  Writeln('Connected');
+  Writeln('Connected'); // inform user of successful connect
 end;
 
 procedure TLSMTPClientTest.OnDisconnect(aSocket: TLSocket);
 begin
-  Writeln('Lost connection');
-  FQuit := True;
+  Writeln('Lost connection'); // inform user about lost connection
+  FQuit := True; // since SMTP shouldn't do this unless we issued a QUIT, consider it to be end of session and quit program
 end;
 
 procedure TLSMTPClientTest.OnError(const msg: string; aSocket: TLSocket);
 begin
-  Writeln(msg);
+  Writeln(msg); // inform about error
 end;
 
 constructor TLSMTPClientTest.Create;
 begin
   FQuit := False;
   FSMTP := TLSMTPClient.Create(nil);
-  FSMTP.OnReceive := @OnReceive;
+  FSMTP.OnReceive := @OnReceive; // assign all events
   FSMTP.OnConnect := @OnConnect;
   FSMTP.OnDisconnect := @OnDisconnect;
   FSMTP.OnError := @OnError;
@@ -113,18 +119,19 @@ var
   Port: Word = 25;
 begin
   if ParamCount > 0 then begin
-    Addr := ParamStr(1);
+    Addr := ParamStr(1); // get address and port from commandline args
     if ParamCount > 1 then
       Port := Word(StrToInt(ParamStr(2)));
 
-    Write('Connecting to ', Addr, '... ');
-    if FSMTP.Connect(Addr, Port) then repeat  // try to connect
+    Write('Connecting to ', Addr, '... '); 
+    if FSMTP.Connect(Addr, Port) then repeat  // try to connect 
       FSMTP.CallAction;  // if inital connect went ok, wait for "acknowlidgment" or otherwise
       if KeyPressed then
         if ReadKey = #27 then
           FQuit := True;  // if user doesn't wish to wait, quit
       Sleep(1);
     until FQuit or FSMTP.Connected; // if user quit, or we connected, then continue
+
     if not FQuit then begin // if we connected send HELO
       FSMTP.Helo;
       Writeln('Press escape to quit or any other key to compose an email');
@@ -132,20 +139,20 @@ begin
 
     while not FQuit do begin // if we connected, do main loop
       SetLength(Recipients, MAX_RECIPIENTS);
-      FSMTP.CallAction;
+      FSMTP.CallAction; // main event mechanism, make sure to call periodicly and ASAP, or specify high timeout
       if KeyPressed then
-        if ReadKey = #27 then begin
-          if FSMTP.Connected then
+        if ReadKey = #27 then begin // if user wants to quit
+          if FSMTP.Connected then // and we're connected, then do a graceful QUIT, waiting for server to disconnect
             FSMTP.Quit
           else
-            FQuit := True;
-        end else begin
-          Sender := GetAnswer('From');
+            FQuit := True; // otherwise just quit from this end
+        end else begin // otherwise, user wants to compose email
+          Sender := GetAnswer('From'); // get info about email from console input
           Recipients := GetAnswer('Recipients');
           Subject := GetAnswer('Subject');
           Message := GetAnswer('Data');
 
-          FSMTP.SendMail(Sender, Recipients, Subject, Message);
+          FSMTP.SendMail(Sender, Recipients, Subject, Message); // send the mail given user data
         end;
       Sleep(1);
     end;
