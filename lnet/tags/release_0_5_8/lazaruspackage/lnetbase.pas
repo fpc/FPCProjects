301cddb67da8aae2e397424a2a41c8a0f36fae0a
Almindor <almindor@gmail.com>
2007-09-16 09:26:00 +0000

    * tag 0.5.8
    git-svn-id: @1834 -

---
+++ lnet/tags/release_0_5_8/lazaruspackage/lnetbase.pas
@@ -0,0 +1,16 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit lnetbase; 
+
+interface
+
+uses
+  lNet, lEvents, lCommon, lFTP, lsmtp, lTelnet, lhttp, lwebserver, 
+    lMimeWrapper, lHTTPUtil, lControlStack, lMimeStreams, lMimeTypes, fastcgi, 
+    lfastcgi, lProcess, lSpawnFCGI, lStrBuffer, ltimer; 
+
+implementation
+
+end.
