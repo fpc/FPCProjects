2d6a008eb2f3eaec6016a3cbd0e244441c8971ef
darius <darius@freepascal.org>
2008-05-11 12:25:35 +0000

    added some more tests to roberts test file
    git-svn-id: @2114 -

--- imagelib/trunk/examples/roberts.pp
+++ imagelib/trunk/examples/roberts.pp
@@ -7,7 +7,6 @@ uses
 
 var
   ci, image : TFPMemoryImage;
-  grayImage : TFPMemoryImage;
   writer : TFPCustomImageWriter;
   reader : TFPCustomImageReader;
   start: TDateTime;
@@ -22,14 +21,21 @@ begin
     writeln('Loading JPEG file from disk');
     ci.LoadFromFile ('lenna_color.jpg', reader);
 
-    start := Now;
-    write('Converting to grayscale...');
-    grayImage := imagelib_rgb_to_grayscale(ci);
-    writeln('done in ', round((Now-start)*24*3600*1000), ' msec');
+    //start := Now;
+    //write('Converting to grayscale........ ');
+    //image := imagelib_rgb_to_grayscale(ci);
+    //image.SaveToFile ('lenna_grayscale.jpg', writer);
+    //writeln('done in ', round((Now-start)*24*3600*1000), ' msec');
+
+    //start := Now;
+    //write('Equalizing histogram........... ');
+    //image := imagelib_equalize_histogram(image);
+    //image.SaveToFile ('lenna_roberts.jpg', writer);
+    //writeln('done in ', round((Now-start)*24*3600*1000), ' msec');
 
     start := Now;
-    write('Calculating Roberts gradient...');
-    image := imagelib_roberts_gradient(grayImage);
+    write('Calculating Roberts gradient... ');
+    image := imagelib_roberts_gradient(ci);
     image.SaveToFile ('lenna_roberts.jpg', writer);
     writeln('done in ', round((Now-start)*24*3600*1000), ' msec');
 
@@ -37,7 +43,6 @@ begin
     image.Free;
     writer.Free;
     ci.free;
-    grayImage.Free;
     reader.Free;
   end;
   
