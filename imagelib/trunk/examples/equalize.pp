297624e3d3d18a9950fd7a28f81ccf1feb34f0a8
darius <darius@freepascal.org>
2008-05-11 12:24:19 +0000

    implemented histogram equalization
    git-svn-id: @2113 -

---
+++ imagelib/trunk/examples/equalize.pp
@@ -0,0 +1,43 @@
+program equalize;
+
+{$mode objfpc}{$H+}
+
+uses 
+  SysUtils, imagelib, FPImage, FPWriteJPEG, FPReadJPEG;
+
+var
+  ci, image : TFPMemoryImage;
+  writer : TFPCustomImageWriter;
+  reader : TFPCustomImageReader;
+  start: TDateTime;
+begin
+  ci := TFPMemoryImage.Create(0,0);
+  ci.UsePalette := False;
+  
+  reader := TFPReaderJPEG.Create;
+  writer := TFPWriterJPEG.Create;
+
+  try
+    writeln('Loading JPEG file from disk');
+    ci.LoadFromFile ('lenna_color.jpg', reader);
+
+    start := Now;
+    write('Converting to grayscale... ');
+    image := imagelib_rgb_to_grayscale(ci);
+    writeln('done in ', round((Now-start)*24*3600*1000), ' msec');
+
+    start := Now;
+    write('Equalizing histogram...... ');
+    image := imagelib_equalize_histogram(image);
+    image.SaveToFile ('lenna_grayscale_equalized.jpg', writer);
+    image.Free;
+    writeln('done in ', round((Now-start)*24*3600*1000), ' msec');
+
+  finally
+    writer.Free;
+    ci.free;
+    reader.Free;
+  end;
+  writeln('done.');
+  readln;
+end.
--- imagelib/trunk/src/imagelib.pas
+++ imagelib/trunk/src/imagelib.pas
@@ -16,16 +16,20 @@ function imagelib_roberts_gradient(ASource: TFPMemoryImage): TFPMemoryImage;
 //utils.inc
 function imagelib_add_edge(ASource: TFPMemoryImage; ASize: integer; const AColor: TFPColor): TFPMemoryImage;
 
-//conversion
+//conversion.inc
 function imagelib_rgb_to_bw(ASource: TFPMemoryImage): TFPMemoryImage;
 function imagelib_rgb_to_grayscale(ASource: TFPMemoryImage): TFPMemoryImage;
 function imagelib_rgb_to_sepia(ASource: TFPMemoryImage): TFPMemoryImage;
 function imagelib_rgb_to_luma(ASource: TFPMemoryImage): TFPMemoryImage;
 
+//histogram.inc
+function imagelib_equalize_histogram(ASource: TFPMemoryImage): TFPMemoryImage;
+
 implementation
 
 {$i segment.inc}
 {$i conversion.inc}
+{$i histogram.inc}
 {$i utils.inc}
 
 begin
