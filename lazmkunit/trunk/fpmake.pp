60c08d54ac188db74287f18df5434d860363084e
joost <joost@cnoc.nl>
2016-07-09 19:54:32 +0000

    * Added lazmkunit to public repository
    git-svn-id: @2696 -

---
+++ lazmkunit/trunk/fpmake.pp
@@ -0,0 +1,38 @@
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit, sysutils;
+
+Var
+  P : TPackage;
+  T : TTarget;
+
+begin
+  With Installer do
+    begin
+    P:=AddPackage('lazmkunit');
+
+    P.Version:='0.9.4-1';
+    P.OSes:=AllUnixOSes+[Win32,Win64];
+    P.Author := 'Joost van der Sluis';
+    P.License := 'LGPL with modification';
+    P.HomepageURL := 'http://www.lazarussupport.nl/';
+    P.Email := 'joost@cnoc.nl';
+    P.Description := 'Tools to use fppkg with Lazarus';
+    P.Options.add('-Sm');
+    P.IsFPMakeAddIn:=True;
+
+    P.Dependencies.Add('fpmkunit');
+    // These dependency is a dependency and should not be added here, but because
+    // of a bug in fpcmake which doesn't detect the dependencies right, this
+    // has to be added temporarily
+    P.Dependencies.Add('fcl-process');
+    P.Dependencies.Add('paszlib');
+    P.Dependencies.Add('hash');
+
+    T:=P.Targets.AddUnit('lazmkunit.pp');
+
+    Run;
+    end;
+end.
+
---
+++ lazmkunit/trunk/lazmkunit.pp
@@ -0,0 +1,323 @@
+unit lazmkunit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpmkunit;
+
+type
+
+  { TLazPackageFile }
+
+  TLazType = (ltFile, ltPackage, ltPackageTemplate);
+
+  TLazPackageFile = Class(TNamedItem)
+  private
+    FInstallSourcePath : string;
+    FLazType: TLazType;
+    function GetInstallSourcePath: string;
+  public
+    Procedure GetInstallFiles(List : TStrings); virtual;
+    property InstallSourcePath : string read GetInstallSourcePath;
+    property LazType: TLazType read FLazType write FLazType;
+  end;
+
+  { TLazPackageFiles }
+
+  TLazPackageFiles = Class(TNamedCollection)
+  private
+    FPackage: TPackage;
+    function GetPackageFileItem(Index : Integer): TLazPackageFile;
+    procedure SetPackageFileItem(Index : Integer; const AValue: TLazPackageFile);
+  public
+    Function AddLazFile(const AFiles : String) : TLazPackageFile;
+    Function AddLazFile(const AFiles : String; AInstallSourcePath : String) : TLazPackageFile;
+    Function AddLazPackageFile(const AFiles : String) : TLazPackageFile;
+    Function AddLazPackageFile(const AFiles : String; AInstallSourcePath : String) : TLazPackageFile;
+    procedure AddLazFiles(const AFileMask: string; Recursive: boolean = False; AInstallSourcePath : String = '');
+    Function AddLazPackageTemplate(const AFiles : String) : TLazPackageFile;
+    Function AddLazPackageTemplate(const AFiles : String; AInstallSourcePath : String) : TLazPackageFile;
+    Property LazPackageFileItem[Index : Integer] : TLazPackageFile Read GetPackageFileItem Write SetPackageFileItem;default;
+    property Package: TPackage read FPackage;
+  end;
+
+  { TLazPackage }
+
+  TLazPackage = Class(TPackage)
+  private
+    FLazPackageFiles: TLazPackageFiles;
+  protected
+    procedure SaveUnitConfigToStringList(const AStringList: TStrings; ACPU: TCPU; AOS: TOS); override;
+  public
+    constructor Create(ACollection: TCollection); override;
+    destructor Destroy; override;
+    procedure GetInstallSourceFiles(List: TStrings; SourceTypes : TSourceTypes; TargetTypes : TTargetTypes); override;
+    Procedure GetArchiveSourceFiles(List : TStrings); override;
+    Property LazPackageFiles : TLazPackageFiles Read FLazPackageFiles;
+  end;
+
+  { TLazPackages }
+
+  TLazPackages = Class(TPackages)
+  private
+    function GetPackage(const AName : String): TLazPackage;
+    function GetPackageItem(AIndex : Integer): TLazPackage;
+    procedure SetPackageItem(AIndex : Integer; const AValue: TLazPackage);
+  Public
+    Function AddPackage(Const AName : String) : TLazPackage;
+    Property Packages[AName : String] : TLazPackage Read GetPackage ; Default;
+    Property PackageItems[AIndex : Integer] : TLazPackage Read GetPackageItem Write SetPackageItem;
+  end;
+
+  { TLazInstaller }
+
+  TLazInstaller = class(TCustomInstaller)
+  private
+    FPackages: TLazPackages;
+  protected
+    Procedure CreatePackages; override;
+    procedure FreePackages; override;
+    function GetPackages: TPackages; override;
+  public
+    procedure DoRegisterLazarusPackages(Sender: TObject);
+    constructor Create(AOwner: TComponent); override;
+  end;
+
+resourcestring
+  SErrAlreadyInitialized  = 'Installer can only be initialized once.';
+  SWarnLazarusDirNotFound = 'Lazarus directory ''%s'' does not exist. Lazarus-package registration skipped.';
+  SWarnLazarusDirInvalid  = 'Lazarus directory ''%s'' does not contain a valid Lazarus installation. Lazarus-package registration skipped.';
+
+implementation
+
+procedure TLazPackage.SaveUnitConfigToStringList(const AStringList: TStrings; ACPU: TCPU; AOS: TOS);
+var
+  i: Integer;
+  L: TStrings;
+begin
+  inherited SaveUnitConfigToStringList(AStringList, ACPU, AOS);
+  L := TStringList.Create;
+  try
+    for i := 0 to LazPackageFiles.Count-1 do
+      begin
+        if LazPackageFiles.LazPackageFileItem[i].FLazType in [ltPackage,ltPackageTemplate] then
+          L.Add(IncludeTrailingPathDelimiter(LazPackageFiles.LazPackageFileItem[i].InstallSourcePath)+ChangeFileExt(ExtractFileName(LazPackageFiles.LazPackageFileItem[i].Name),'.lpk'));
+      end;
+    if L.Count>0 then
+      AStringList.Values['LazarusPackageFiles']:=L.CommaText;
+  finally
+    L.Free;
+  end;
+  AStringList.Values[''];
+end;
+
+constructor TLazPackage.Create(ACollection: TCollection);
+begin
+  inherited Create(ACollection);
+  FLazPackageFiles := TLazPackageFiles.Create(TLazPackageFile);
+  FLazPackageFiles.FPackage := self;
+end;
+
+destructor TLazPackage.Destroy;
+begin
+  FreeAndNil(FLazPackageFiles);
+  inherited Destroy;
+end;
+
+procedure TLazPackage.GetInstallSourceFiles(List: TStrings; SourceTypes : TSourceTypes; TargetTypes : TTargetTypes);
+Var
+  I : Integer;
+begin
+  inherited GetInstallSourceFiles(List, SourceTypes, TargetTypes);
+  if stDoc in SourceTypes then
+    For I:=0 to FLazPackageFiles.Count-1 do
+      FLazPackageFiles.LazPackageFileItem[I].GetInstallFiles(List);
+end;
+
+procedure TLazPackage.GetArchiveSourceFiles(List: TStrings);
+var
+  i : integer;
+begin
+  inherited GetArchiveSourceFiles(List);
+  for i := 0 to LazPackageFiles.Count-1 do
+    List.Add(LazPackageFiles[i].Name);
+end;
+
+{ TLazPackageFiles }
+
+function TLazPackageFiles.GetPackageFileItem(Index : Integer): TLazPackageFile;
+begin
+  Result:=TLazPackageFile(Items[Index]);
+end;
+
+procedure TLazPackageFiles.SetPackageFileItem(Index : Integer; const AValue: TLazPackageFile);
+begin
+  Items[Index]:=AValue;
+end;
+
+function TLazPackageFiles.AddLazFile(const AFiles: String): TLazPackageFile;
+begin
+  Result:=Add as TLazPackageFile;
+  Result.Name:=AFiles;
+end;
+
+function TLazPackageFiles.AddLazFile(const AFiles: String; AInstallSourcePath: String): TLazPackageFile;
+begin
+  Result:=Add as TLazPackageFile;
+  Result.Name:=AFiles;
+  Result.FInstallSourcePath:=AInstallSourcePath;
+end;
+
+function TLazPackageFiles.AddLazPackageFile(const AFiles: String): TLazPackageFile;
+begin
+  result := AddLazFile(AFiles);
+  result.FLazType:=ltPackage;
+end;
+
+function TLazPackageFiles.AddLazPackageFile(const AFiles: String; AInstallSourcePath: String): TLazPackageFile;
+begin
+  result := AddLazFile(AFiles, AInstallSourcePath);
+  result.FLazType:=ltPackage;
+end;
+
+procedure TLazPackageFiles.AddLazFiles(const AFileMask: string; Recursive: boolean; AInstallSourcePath: String);
+var
+  List : TStrings;
+  i: integer;
+begin
+  List := TStringList.Create;
+  SearchFiles(AFileMask, '', Recursive, List);
+  for i:= 0 to Pred(List.Count) do
+    AddLazFile(List[i], AInstallSourcePath);
+  List.Free;
+end;
+
+function TLazPackageFiles.AddLazPackageTemplate(const AFiles: String): TLazPackageFile;
+begin
+  result := AddLazFile(AFiles);
+  result.FLazType:=ltPackageTemplate;
+end;
+
+function TLazPackageFiles.AddLazPackageTemplate(const AFiles: String; AInstallSourcePath: String): TLazPackageFile;
+begin
+  result := AddLazFile(AFiles, AInstallSourcePath);
+  result.FLazType:=ltPackageTemplate;
+end;
+
+{ TLazPackageFile }
+
+function TLazPackageFile.GetInstallSourcePath: string;
+begin
+  if (FInstallSourcePath<>'') and not IsRelativePath(FInstallSourcePath) then
+    result := FInstallSourcePath
+  else If Defaults.UnixPaths then
+    result:=Defaults.Prefix+'share'+PathDelim+'fpc-'+IncludeTrailingPathDelimiter((Collection as TLazPackageFiles).Package.Name)+FInstallSourcePath
+  else
+    result:=Defaults.BaseInstallDir+PathDelim+'fpc-'+IncludeTrailingPathDelimiter((Collection as TLazPackageFiles).Package.Name)+FInstallSourcePath;
+end;
+
+procedure TLazPackageFile.GetInstallFiles(List: TStrings);
+begin
+  list.Values[name] := (IncludeTrailingPathDelimiter(InstallSourcePath)+ExtractFileName(Name))
+end;
+
+{ TLazPackages }
+
+function TLazPackages.GetPackage(const AName: String): TLazPackage;
+begin
+  Result:=TLazPackage(ItemByName(AName))
+end;
+
+function TLazPackages.GetPackageItem(AIndex: Integer): TLazPackage;
+begin
+  Result:=TLazPackage(Items[AIndex]);
+end;
+
+procedure TLazPackages.SetPackageItem(AIndex: Integer; const AValue: TLazPackage);
+begin
+  Items[AIndex]:=AValue;
+end;
+
+function TLazPackages.AddPackage(const AName: String): TLazPackage;
+begin
+  Result:=Add as TLazPackage;
+  Result.Name:=AName;
+end;
+
+{ TLazInstaller }
+
+procedure TLazInstaller.CreatePackages;
+begin
+  FPackages:=TLazPackages.Create(TLazPackage);
+end;
+
+procedure TLazInstaller.FreePackages;
+begin
+  FreeAndNil(FPackages);
+end;
+
+function TLazInstaller.GetPackages: TPackages;
+begin
+  Result:=FPackages;
+end;
+
+procedure TLazInstaller.DoRegisterLazarusPackages(Sender: TObject);
+Var
+  LazarusDir : string;
+  LazPackagerFile : Text;
+  APackage: TLazPackage;
+  i: integer;
+  PackageFileName: string;
+  LazCompiledFileName: string;
+  LazGlobalLinksDir: string;
+
+begin
+  LazarusDir := ExpandFileName(GetCustomFpmakeCommandlineOptionValue('lazarusdir'));
+  if LazarusDir <> '' then
+    begin
+    APackage := sender as TLazPackage;
+    LazGlobalLinksDir:=IncludeTrailingPathDelimiter(LazarusDir)+'packager'+PathDelim+'globallinks'+PathDelim;
+    for i := 0 to APackage.LazPackageFiles.Count-1 do with APackage.LazPackageFiles.LazPackageFileItem[i] do
+      if LazType in [ltPackage, ltPackageTemplate] then
+        begin
+        if not DirectoryExists(LazarusDir) then
+          log(vlWarning,Format(SWarnLazarusDirNotFound,[LazarusDir]))
+        else if not DirectoryExists(LazGlobalLinksDir) then
+          log(vlWarning,Format(SWarnLazarusDirInvalid,[LazarusDir]))
+        else
+          begin
+          PackageFileName := IncludeTrailingPathDelimiter(InstallSourcePath)+ExtractFileName(Name);
+          if LazType = ltPackageTemplate then
+            BuildEngine.CmdRenameFile(PackageFileName,ChangeFileExt(PackageFileName,'.lpk'));
+          System.assign(LazPackagerFile,LazGlobalLinksDir+ChangeFileExt(ExtractFileName(Name),'')+'-0.lpl');
+          System.Rewrite(LazPackagerFile);
+          System.WriteLn(LazPackagerFile,ChangeFileExt(PackageFileName,'.lpk'));
+          System.close(LazPackagerFile);
+
+          // If the package is already compiled by Lazarus, clear the .compiled file, to force a recompile by Lazarus
+
+          // This only works if the package UnitOutputDirectory is set to the default "lib/$(TargetCPU)-$(TargetOS)"
+          // Better would be to parse the .lpk for the Compileroptions.UnitOutputDirecory and then resolve the used
+          // macros. For now this is an easy hack which will work in most cases.
+          LazCompiledFileName := IncludeTrailingPathDelimiter(InstallSourcePath)+'lib' + PathDelim + CurrentCPU + '-' + CurrentOS + PathDelim + ExtractFileName(Name);
+          LazCompiledFileName := ChangeFileExt(LazCompiledFileName,'.compiled');
+          if FileExists(LazCompiledFileName) then
+            DeleteFile(LazCompiledFileName);
+          end;
+        end;
+    end;
+end;
+
+constructor TLazInstaller.Create(AOwner: TComponent);
+begin
+  AddCustomFpmakeCommandlineOption('lazarusdir','Location of a Lazarus installation.');
+  if assigned(Defaults) then
+    Error(SErrAlreadyInitialized);
+  Defaults:=TFPCDefaults.Create;
+  inherited Create(AOwner);
+end;
+
+end.
+
