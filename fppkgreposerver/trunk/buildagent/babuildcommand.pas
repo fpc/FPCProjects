05f036610350a7e1c443d38a9e0800e98b0a03df
joost <joost@cnoc.nl>
2017-12-04 22:36:14 +0000

    * Added the manifest and upload commands
    git-svn-id: @2769 -

--- fppkgreposerver/trunk/buildagent/babuildcommand.pas
+++ fppkgreposerver/trunk/buildagent/babuildcommand.pas
@@ -7,69 +7,229 @@ interface
 uses
   Classes,
   SysUtils,
+  zipper,
   HTTPDefs,
   FileUtil,
+  fpjson,
+  DOM,
+  XMLRead,
   dcsHandler,
   dcsThreadCommandFactory,
+  dcsInOutputProcessor,
   DCSHTTPRestServer,
+  fprPackageUtils,
   baCommand;
 
 type
 
-  { TbaBuildCommand }
+  { TbaCustomBuildCommand }
 
-  TbaBuildCommand = class(TbaCustomCommand)
+  TbaCustomBuildCommand = class(TbaCustomCommand)
   protected
     FTempArchiveFileName: string;
+    procedure PrepareBuildEnvironment(BuildPath: string; ArchiveName: string);
   public
-    class function TextName: string; override;
-    destructor Destroy; override;
-    function DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean; override;
     procedure FillCommandBasedOnRequest(ARequest: TRequest); override;
+    destructor Destroy; override;
   published
     property OSTarget;
     property CPUTarget;
     property FPCVersion;
   end;
 
+  { TbaBuildCommand }
+
+  TbaBuildCommand = class(TbaCustomBuildCommand)
+  public
+    function DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean; override;
+    class function TextName: string; override;
+  end;
+
+  { TDCSManifestNotificationEvent }
+
+  TDCSManifestNotificationEvent = class(TDCSNotificationEvent, IDCSJSonResultEvent)
+  private
+    FManifest: TJSONArray;
+  public
+    procedure AfterEventToJSon(Self: TDCSJSonInOutputProcessor; AnEvent: TDCSEvent; JSonEvent: TJSONObject);
+    destructor Destroy; override;
+  published
+    property Manifest: TJSONArray read FManifest write FManifest;
+  end;
+
+
+  { TbaCustomManifestCommand }
+
+  TbaCustomManifestCommand = class(TbaCustomBuildCommand)
+  protected
+    FZipOutputPath: string;
+  public
+    function DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean; override;
+  end;
+
+  { TbaManifestCommand }
+
+  TbaManifestCommand = class(TbaCustomManifestCommand)
+  protected
+    FManifestJSON: TJSONArray;
+    function GetNotificationCommandEventClass: TDCSNotificationEventClass; override;
+    function CreateExecutedCommandEvent(Success: Boolean; ReturnMessage: string; NotificationClass: TDCSNotificationEventClass): TDCSNotificationEvent; override;
+  public
+    destructor Destroy; override;
+    function DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean; override;
+    class function TextName: string; override;
+  end;
+
+  { TbaUploadCommand }
+
+  TbaUploadCommand = class(TbaCustomManifestCommand)
+  public
+    function DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean; override;
+    class function TextName: string; override;
+  end;
 
 implementation
 
 uses
   baBuildFPCEnvironment;
 
-{ TbaBuildCommand }
+{ TbaCustomManifestCommand }
 
-class function TbaBuildCommand.TextName: string;
+function TbaCustomManifestCommand.DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean;
+var
+  BuildPath: String;
+  ArchiveName: RawByteString;
+  UnZipper: TUnZipper;
+  ManifestXML: TXMLDocument;
 begin
-  Result := 'build';
+  Result := False;
+  BuildPath := GetBuildPath;
+  ArchiveName := ConcatPaths([BuildPath, ChangeFileExt(ExtractFileName(FTempArchiveFileName), '.zip')]);
+  FZipOutputPath := ConcatPaths([BuildPath, 'tmppackage']);
+
+  PrepareBuildEnvironment(BuildPath, ArchiveName);
+
+  try
+    UnZipper := TUnZipper.Create;
+    try
+      UnZipper.FileName := ArchiveName;
+      UnZipper.OutputPath := FZipOutputPath;
+      UnZipper.UnZipAllFiles();
+    finally
+      UnZipper.Free;
+    end;
+
+  except
+    on E: Exception do
+    begin
+    ReturnMessage := 'Failed to extract zip-file. ' + E.Message;
+    Exit;
+    end;
+  end;
+
+  RunTestCommandIndir(FZipOutputPath, GetFppkgExecutable, ['-C', ConcatPaths([BuildPath, 'etc', 'fppkg.cfg']), 'archive'], 'create source-archive for package');
+
+  ReturnMessage := '';
+  Result := true;
 end;
 
-destructor TbaBuildCommand.Destroy;
+{ TbaUploadCommand }
+
+function TbaUploadCommand.DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean;
+var
+  ManifestXML: TXMLDocument;
 begin
-  DeleteFile(FTempArchiveFileName);
+  Result := inherited DoExecute(AController, ReturnMessage);
+  if Result then
+    begin
+    ReadXMLFile(ManifestXML, ConcatPaths([FZipOutputPath, 'manifest.xml']));
+    try
+
+    finally
+      ManifestXML.Free;
+    end;
+    end;
+
+  ReturnMessage := '';
+  Result := true;
+end;
+
+class function TbaUploadCommand.TextName: string;
+begin
+  Result := 'upload';
+end;
+
+{ TDCSManifestNotificationEvent }
+
+procedure TDCSManifestNotificationEvent.AfterEventToJSon(Self: TDCSJSonInOutputProcessor; AnEvent: TDCSEvent; JSonEvent: TJSONObject);
+begin
+  if Assigned(Manifest) then
+    JSonEvent.Add('manifest', Manifest.Clone);
+end;
+
+destructor TDCSManifestNotificationEvent.Destroy;
+begin
+  FManifest.Free;
   inherited Destroy;
 end;
 
+{ TbaManifestCommand }
+
+function TbaManifestCommand.GetNotificationCommandEventClass: TDCSNotificationEventClass;
+begin
+  Result := TDCSManifestNotificationEvent;
+end;
+
+function TbaManifestCommand.CreateExecutedCommandEvent(Success: Boolean; ReturnMessage: string;
+  NotificationClass: TDCSNotificationEventClass): TDCSNotificationEvent;
+begin
+  Result := inherited CreateExecutedCommandEvent(Success, ReturnMessage, NotificationClass);
+  (result as TDCSManifestNotificationEvent).Manifest := FManifestJSON;
+  FManifestJSON := nil;
+end;
+
+destructor TbaManifestCommand.Destroy;
+begin
+  if assigned(FManifestJSON) then
+    FManifestJSON.Free;
+  inherited Destroy;
+end;
+
+function TbaManifestCommand.DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean;
+var
+  ManifestXML: TXMLDocument;
+begin
+  Result := inherited DoExecute(AController, ReturnMessage);
+  if Result then
+    begin
+    ReadXMLFile(ManifestXML, ConcatPaths([FZipOutputPath, 'manifest.xml']));
+    try
+      FManifestJSON := ManifestToJSON(ManifestXML);
+    finally
+      ManifestXML.Free;
+    end;
+    end;
+
+  ReturnMessage := '';
+  Result := true;
+end;
+
+class function TbaManifestCommand.TextName: string;
+begin
+  Result := 'manifest';
+end;
+
+{ TbaBuildCommand }
+
 function TbaBuildCommand.DoExecute(AController: TDCSCustomController; out ReturnMessage: string): Boolean;
 var
-  PristineEnvironmentPath, BuildPath: String;
-  ArchiveName: string;
+  BuildPath: String;
+  ArchiveName: RawByteString;
 begin
-  PristineEnvironmentPath := GetPristineEnvironmentPath;
-  if not DirectoryExists(PristineEnvironmentPath) then
-    raise Exception.CreateFmt('No pristine FPC-installation available (%s). Please run  %s command first.', [PristineEnvironmentPath, TbaBuildFPCEnvironment.TextName]);
-
   BuildPath := GetBuildPath;
-
-  FDistributor.Log(Format('Create build-environment at (%s), based on pristine FPC-installation at (%s)', [BuildPath, PristineEnvironmentPath]), etInfo, Null, FSendByLisId);
-
-  ForceDirectories(BuildPath);
-  RunTestCommandIndir(PristineEnvironmentPath, 'rsync', ['-rtvul', '--delete', PristineEnvironmentPath, BuildPath], 'sync FPC-installation');
   ArchiveName := ConcatPaths([BuildPath, ChangeFileExt(ExtractFileName(FTempArchiveFileName), '.zip')]);
 
-  if not CopyFile(FTempArchiveFileName, ArchiveName) then
-    raise Exception.CreateFmt('Failed to copy archive (%s) to build-environment (%s).', [FTempArchiveFileName, ArchiveName]);
+  PrepareBuildEnvironment(BuildPath, ArchiveName);
 
   RunTestCommandIndir(BuildPath, GetFppkgExecutable, ['-C', ConcatPaths([BuildPath, 'etc', 'fppkg.cfg']), 'build', ArchiveName], 'build package');
 
@@ -77,7 +237,37 @@ begin
   Result := true;
 end;
 
-procedure TbaBuildCommand.FillCommandBasedOnRequest(ARequest: TRequest);
+class function TbaBuildCommand.TextName: string;
+begin
+  Result := 'build';
+end;
+
+{ TbaCustomBuildCommand }
+
+destructor TbaCustomBuildCommand.Destroy;
+begin
+  DeleteFile(FTempArchiveFileName);
+  inherited Destroy;
+end;
+
+procedure TbaCustomBuildCommand.PrepareBuildEnvironment(BuildPath: string; ArchiveName: string);
+var
+  PristineEnvironmentPath: String;
+begin
+  PristineEnvironmentPath := GetPristineEnvironmentPath;
+  if not DirectoryExists(PristineEnvironmentPath) then
+    raise Exception.CreateFmt('No pristine FPC-installation available (%s). Please run  %s command first.', [PristineEnvironmentPath, TbaBuildFPCEnvironment.TextName]);
+
+  FDistributor.Log(Format('Create build-environment at (%s), based on pristine FPC-installation at (%s)', [BuildPath, PristineEnvironmentPath]), etInfo, Null, FSendByLisId);
+
+  ForceDirectories(BuildPath);
+  RunTestCommandIndir(PristineEnvironmentPath, 'rsync', ['-rtvul', '--delete', PristineEnvironmentPath, BuildPath], 'sync FPC-installation');
+
+  if not CopyFile(FTempArchiveFileName, ArchiveName) then
+    raise Exception.CreateFmt('Failed to copy archive (%s) to build-environment (%s).', [FTempArchiveFileName, ArchiveName]);
+end;
+
+procedure TbaCustomBuildCommand.FillCommandBasedOnRequest(ARequest: TRequest);
 var
   fs: TFileStream;
 begin
@@ -93,5 +283,7 @@ end;
 
 initialization
   TDCSThreadCommandFactory.RegisterCommandClass(TbaBuildCommand);
+  TDCSThreadCommandFactory.RegisterCommandClass(TbaManifestCommand);
+  TDCSThreadCommandFactory.RegisterCommandClass(TbaUploadCommand);
 end.
 
