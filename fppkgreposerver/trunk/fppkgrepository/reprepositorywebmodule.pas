6fd087c3faaf946b1491ab8d3a3969f2361f69e7
joost <joost@cnoc.nl>
2018-05-01 00:26:04 +0000

    * Handle master-repository
    git-svn-id: @2819 -

--- fppkgreposerver/trunk/fppkgrepository/reprepositorywebmodule.pas
+++ fppkgreposerver/trunk/fppkgrepository/reprepositorywebmodule.pas
@@ -23,6 +23,7 @@ uses
   fprDeleteTree,
   fprCopyTree,
   fprErrorHandling,
+  fprGCollection,
   repPackage;
 
 type
@@ -33,7 +34,10 @@ type
     procedure DataModuleCreate(Sender: TObject);
     Procedure DataModuleRequest(Sender: TObject; ARequest: TRequest; AResponse: TResponse; Var Handled: Boolean);
   private
-    function RebuildRepository(ARepository: TrepRepository): string;
+    function RebuildRepository(AFPCVersion: TrepFPCVersion; ARepository: TrepRepository): string;
+  protected
+    procedure RebuildPackage(APackage: TrepPackage; AManifest: TXMLDocument; APackagesNode: TDOMElement;
+      ARepositoryURL, ABuildAgentURL, ARepoTempPath: string);
   end;
 
 var
@@ -87,7 +91,7 @@ begin
         if ARequest.GetNextPathInfo = '' then
           AResponse.Content := ObjectToJSONContentString( Repository.PackageList )
         else
-          AResponse.Content := RebuildRepository(Repository);
+          AResponse.Content := RebuildRepository(FPCVersion, Repository);
 
         end;
       end;
@@ -97,29 +101,24 @@ begin
   Handled := True;
 end;
 
-function TrepRepositoryWM.RebuildRepository(ARepository: TrepRepository): string;
+function TrepRepositoryWM.RebuildRepository(AFPCVersion: TrepFPCVersion; ARepository: TrepRepository
+  ): string;
 var
   i: Integer;
   RepositoryURL: string;
   Package: TrepPackage;
   RepoTempPath: string;
-  HTTPClient: TFPHTTPClient;
-  BytesStream: TBytesStream;
-  Resp, BuildAgentURL: string;
-  BuildResponseList: TfprBuildAgentResponseList;
-  BuildResponse: TfprBuildAgentResponse;
-  FileStream: TFileStream;
+  BuildAgentURL: string;
   ManifestStream: TStringStream;
   Manifest: TXMLDocument;
   Mirrors: TXMLDocument;
   MirrorNode: TDOMElement;
   MirrorsNode: TDOMElement;
   MirrorChildNode: TDOMElement;
-  PackageManifest: TXMLDocument;
-  PackagePackagesNode, PackageNode: TDOMNode;
   PackagesNode: TDOMElement;
   RepositoryNode: TDOMElement;
-  RepoPath, Url: string;
+  RepoPath: string;
+  MasterRepository: TrepRepository;
 begin
   Result := '';
   if Arepository.Path='' then
@@ -138,94 +137,25 @@ begin
     RepoTempPath := GetTempFileName(GetTempDir(False), 'repodir');
     ForceDirectories(RepoTempPath);
     try
+      if ARepository.MasterRepositoryName<>'' then
+        begin
+        MasterRepository := AFPCVersion.RepositoryList.FindRepository(ARepository.MasterRepositoryName);
+        if not Assigned(MasterRepository) then
+          raise EHTTPClient.CreateFmtHelp('Master repository %s does not exist', [ARepository.MasterRepositoryName] , 400);
+
+        for i := 0 to MasterRepository.PackageList.Count -1 do
+          begin
+          Package := MasterRepository.PackageList.Items[i];
+          if not Assigned(ARepository.PackageList.FindPackage(Package.Name)) then
+            RebuildPackage(Package, Manifest, PackagesNode, RepositoryURL, BuildAgentURL, RepoTempPath);
+          end;
+        end;
+
       for i := 0 to ARepository.PackageList.Count -1 do
         begin
         Package := ARepository.PackageList.Items[i];
 
-        try
-          HTTPClient := TFPHTTPClient.Create(nil);
-          try
-            HTTPClient.RequestHeaders.Values['authorization'] := 'Bearer ' + FAccessToken;
-            BytesStream := TBytesStream.Create;
-            try
-              try
-                Url := IncludeHTTPPathDelimiter(RepositoryURL)+'package/'+Package.Name+'/'+Package.Tag;
-                HTTPClient.Get(Url, BytesStream);
-              except
-                on E: Exception do
-                  Raise EHTTPClient.CreateFmtHelp('Exception during downloading ''%s'': %s (%s)', [Url, E.Message, E.ClassName], 500);
-              end;
-              BytesStream.Seek(0, soFromBeginning);
-              HTTPClient.RequestBody := BytesStream;
-              try
-                Url := IncludeHTTPPathDelimiter(BuildAgentURL)+'createsourcearchive?cputarget=x86_64&ostarget=linux&fpcversion=3.1.1&chunked=false';
-                Resp := HTTPClient.Get(Url);
-              except
-                on E: Exception do
-                  Raise EHTTPClient.CreateFmtHelp('Exception during request to create source-archive ''%s'': %s (%s)', [Url, E.Message, E.ClassName], 500);
-              end;
-            finally
-              BytesStream.Free;
-            end;
-          finally
-            HTTPClient.Free;
-          end;
-
-          BuildResponseList := TfprBuildAgentResponseList.Create();
-          try
-            JSONContentStringToObject(Resp, BuildResponseList);
-            if (BuildResponseList.Count > 0) then
-              begin
-              BuildResponse := BuildResponseList.Items[BuildResponseList.Count -1];
-              if BuildResponse.AType <> 'Done' then
-                raise EJsonWebException.CreateFmtHelp('Failed to create source-archive for package %s (%s): %s', [Package.Name, Package.Tag, BuildResponse.Message], 500);
-
-              if BuildResponse.SourceArchive = '' then
-                raise EJsonWebException.CreateFmtHelp('Failed to create source-archive: %s', [BuildResponse.Message], 500);
-
-              HTTPClient := TFPHTTPClient.Create(nil);
-              try
-                HTTPClient.RequestHeaders.Values['authorization'] := 'Bearer ' + FAccessToken;
-                FileStream := TFileStream.Create(ConcatPaths([RepoTempPath, ExtractFileName(BuildResponse.SourceArchive)]), fmCreate);
-                try
-                  HTTPClient.Get(BuildResponse.SourceArchive, FileStream);
-                finally
-                  FileStream.Free;
-                end;
-              finally
-                HTTPClient.Free;
-              end;
-
-              ManifestStream := TStringStream.Create(BuildResponse.ManifestXML);
-              try
-                ReadXMLFile(PackageManifest, ManifestStream);
-                try
-                  PackagePackagesNode := PackageManifest.FindNode('packages');
-                  if not Assigned(PackagePackagesNode) then
-                    raise EJsonWebException.CreateHelp('Manifest does not contain packages.', 500);
-
-                  if PackagePackagesNode.ChildNodes.Count =0 then
-                    raise EJsonWebException.CreateHelp('Manifest does not contain any package.', 500);
-
-                  PackageNode := PackagePackagesNode.ChildNodes[0];
-                  PackageNode := PackageNode.CloneNode(True, Manifest);
-                  PackagesNode.AppendChild(PackageNode);
-                finally
-                  PackageManifest.Free;
-                end;
-              finally
-                ManifestStream.Free;
-              end;
-              end
-            else
-              raise EJsonWebException.CreateFmtHelp('Failed to create source-archive for package [%s]', [Package.Name],500);
-          finally
-            BuildResponseList.Free;
-          end;
-        except
-          on E: Exception do
-            Raise Exception.CreateFmt('Exception during handling package ''%s'' (%s): %s (%s)', [Package.Name, Package.Tag, E.Message, E.ClassName]);
-        end;
+        RebuildPackage(Package, Manifest, PackagesNode, RepositoryURL, BuildAgentURL, RepoTempPath);
         end;
 
       ManifestStream := TStringStream.Create(nil);
@@ -282,6 +212,105 @@ begin
     end;
   finally
     Manifest.Free;
+  end;
+end;
+
+procedure TrepRepositoryWM.RebuildPackage(APackage: TrepPackage; AManifest: TXMLDocument;
+  APackagesNode: TDOMElement; ARepositoryURL, ABuildAgentURL, ARepoTempPath: string);
+var
+  HTTPClient: TFPHTTPClient;
+  BytesStream: TBytesStream;
+  Url, Resp: String;
+  BuildResponseList: TfprBuildAgentResponseList;
+  BuildResponse: TfprBuildAgentResponse;
+  FileStream: TFileStream;
+  ManifestStream: TStringStream;
+  PackageManifest: TXMLDocument;
+  PackagePackagesNode, PackageNode: TDOMNode;
+begin
+  try
+    HTTPClient := TFPHTTPClient.Create(nil);
+    try
+      HTTPClient.RequestHeaders.Values['authorization'] := 'Bearer ' + FAccessToken;
+      BytesStream := TBytesStream.Create;
+      try
+        try
+          Url := IncludeHTTPPathDelimiter(ARepositoryURL)+'package/'+APackage.Name+'/'+APackage.Tag;
+          HTTPClient.Get(Url, BytesStream);
+        except
+          on E: Exception do
+            Raise EHTTPClient.CreateFmtHelp('Exception during downloading ''%s'': %s (%s)', [Url, E.Message, E.ClassName], 500);
+        end;
+        BytesStream.Seek(0, soFromBeginning);
+        HTTPClient.RequestBody := BytesStream;
+        try
+          Url := IncludeHTTPPathDelimiter(ABuildAgentURL)+'createsourcearchive?cputarget=x86_64&ostarget=linux&fpcversion=3.1.1&chunked=false';
+          Resp := HTTPClient.Get(Url);
+        except
+          on E: Exception do
+            Raise EHTTPClient.CreateFmtHelp('Exception during request to create source-archive ''%s'': %s (%s)', [Url, E.Message, E.ClassName], 500);
+        end;
+      finally
+        BytesStream.Free;
+      end;
+    finally
+      HTTPClient.Free;
+    end;
+
+    BuildResponseList := TfprBuildAgentResponseList.Create();
+    try
+      JSONContentStringToObject(Resp, BuildResponseList);
+      if (BuildResponseList.Count > 0) then
+        begin
+        BuildResponse := BuildResponseList.Items[BuildResponseList.Count -1];
+        if BuildResponse.AType <> 'Done' then
+          raise EJsonWebException.CreateFmtHelp('Failed to create source-archive for package %s (%s): %s', [APackage.Name, APackage.Tag, BuildResponse.Message], 500);
+
+        if BuildResponse.SourceArchive = '' then
+          raise EJsonWebException.CreateFmtHelp('Failed to create source-archive: %s', [BuildResponse.Message], 500);
+
+        HTTPClient := TFPHTTPClient.Create(nil);
+        try
+          HTTPClient.RequestHeaders.Values['authorization'] := 'Bearer ' + FAccessToken;
+          FileStream := TFileStream.Create(ConcatPaths([ARepoTempPath, ExtractFileName(BuildResponse.SourceArchive)]), fmCreate);
+          try
+            HTTPClient.Get(BuildResponse.SourceArchive, FileStream);
+          finally
+            FileStream.Free;
+          end;
+        finally
+          HTTPClient.Free;
+        end;
+
+        ManifestStream := TStringStream.Create(BuildResponse.ManifestXML);
+        try
+          ReadXMLFile(PackageManifest, ManifestStream);
+          try
+            PackagePackagesNode := PackageManifest.FindNode('packages');
+            if not Assigned(PackagePackagesNode) then
+              raise EJsonWebException.CreateHelp('Manifest does not contain packages.', 500);
+
+            if PackagePackagesNode.ChildNodes.Count =0 then
+              raise EJsonWebException.CreateHelp('Manifest does not contain any package.', 500);
+
+            PackageNode := PackagePackagesNode.ChildNodes[0];
+            PackageNode := PackageNode.CloneNode(True, AManifest);
+            APackagesNode.AppendChild(PackageNode);
+          finally
+            PackageManifest.Free;
+          end;
+        finally
+          ManifestStream.Free;
+        end;
+        end
+      else
+        raise EJsonWebException.CreateFmtHelp('Failed to create source-archive for package [%s]', [APackage.Name],500);
+    finally
+      BuildResponseList.Free;
+    end;
+  except
+    on E: Exception do
+      Raise Exception.CreateFmt('Exception during handling package ''%s'' (%s): %s (%s)', [APackage.Name, APackage.Tag, E.Message, E.ClassName]);
   end;
 end;
 
