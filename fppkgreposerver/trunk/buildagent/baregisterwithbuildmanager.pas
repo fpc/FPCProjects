dc6d54073303dfb972f0d702bb5c668265eb0b09
joost <joost@cnoc.nl>
2018-08-19 17:05:55 +0000

    * Repeat the registration to the build-manager, so it can restore itself when the build-manager restarts. Use a smaller interval when the registration fails
    git-svn-id: @2858 -

--- fppkgreposerver/trunk/buildagent/baregisterwithbuildmanager.pas
+++ fppkgreposerver/trunk/buildagent/baregisterwithbuildmanager.pas
@@ -7,6 +7,7 @@ interface
 uses
   Classes,
   SysUtils,
+  DateUtils,
   fphttpclient,
   dcsGlobalSettings,
   dcsHandler;
@@ -41,8 +42,10 @@ var
   SectionList: TStringList;
   i: Integer;
   SS: TStringStream;
+  RegistrationSucceeded: Boolean;
 begin
   Result := False;
+  RegistrationSucceeded := False;
   BuildManagerURL := TDCSGlobalSettings.GetInstance.GetSettingAsString('buildmanagerurl');
   AgentName := TDCSGlobalSettings.GetInstance.GetSettingAsString('AgentName');
   AgentURL := TDCSGlobalSettings.GetInstance.GetSettingAsString('AgentURL');
@@ -62,27 +65,35 @@ begin
     begin
     SectionList := TStringList.Create;
     try
-      TDCSGlobalSettings.GetInstance.FillSectionList(SectionList);
-      for i := 0 to SectionList.Count -1 do
-        begin
-        if SameText(copy(SectionList[i], 1, 7), 'TestEnv') then
+      try
+        TDCSGlobalSettings.GetInstance.FillSectionList(SectionList);
+        for i := 0 to SectionList.Count -1 do
           begin
-          HttpClient := TFPHTTPClient.Create(nil);
-          try
-            SS := TStringStream.Create(Format('{"name":"%s","url":"%s","fpcversion":"%s"}', [AgentName+'-'+copy(SectionList[i],9), AgentURL, FPCVersion]));
+          if SameText(copy(SectionList[i], 1, 7), 'TestEnv') then
+            begin
+            HttpClient := TFPHTTPClient.Create(nil);
             try
-              HttpClient.RequestBody := SS;
-              ReturnMessage := HttpClient.Get(BuildManagerURL+'/agent/register');
+              SS := TStringStream.Create(Format('{"name":"%s","url":"%s","fpcversion":"%s"}', [AgentName+'-'+copy(SectionList[i],9), AgentURL, FPCVersion]));
+              try
+                HttpClient.RequestBody := SS;
+                ReturnMessage := HttpClient.Get(BuildManagerURL+'/agent/register');
+                RegistrationSucceeded := True;
+              finally
+                SS.Free;
+              end;
             finally
-              SS.Free;
+              HttpClient.Free;
             end;
-          finally
-            HttpClient.Free;
+            end;
           end;
-          end;
-        end;
+      finally
+        SectionList.Free;
+      end;
     finally
-      SectionList.Free;
+      if RegistrationSucceeded then
+        FDistributor.TimeQueueCommand(TbaRegisterWithBuildManagerCommand.Create(-1, null, FDistributor), IncSecond(Now, 300))
+      else
+        FDistributor.TimeQueueCommand(TbaRegisterWithBuildManagerCommand.Create(-1, null, FDistributor), IncSecond(Now, 10));
     end;
     ReturnMessage := 'Registered with BuildManager';
     Result := True;
