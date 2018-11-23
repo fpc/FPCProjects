61fa2ebba3779f2cf9c62d388662a24290842c2d
joost <joost@cnoc.nl>
2018-11-23 19:54:42 +0000

    * Settings for test-environment
    git-svn-id: @2893 -

---
+++ fppkgreposerver/trunk/webclient/src/environments/environment.testing.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  identityServerUrl: 'http://fpc.cnoc.nl:8080/identityserver',
+  webclientUrl: 'http://fpc.cnoc.nl:8080',
+  buildAgentUrl: 'http://fpc.cnoc.nl:8080/buildagent',
+  packageManagerUrl: 'http://fpc.cnoc.nl:8080/packagemanager',
+  buildManagerUrl: 'http://fpc.cnoc.nl:8080/buildmanager',
+  repositoryUrl:  'http://fpc.cnoc.nl:8080/repository',
+  fppkgRepositoryUrl: 'http://fpc.cnoc.nl:8080/fppkgrepository'
+};
