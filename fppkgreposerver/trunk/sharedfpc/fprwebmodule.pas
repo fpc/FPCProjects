b7d740c1a419ab5be42a1e307719e2a946e60dbf
joost <joost@cnoc.nl>
2019-11-27 16:37:27 +0000

    * Some http-clients also check for the CORS headers on normal request. In those cases we should give the proper CORS-headers back, but not block the whole request. (Question is if we should on OPTIONS-request...)
    git-svn-id: @2958 -

--- fppkgreposerver/trunk/sharedfpc/fprwebmodule.pas
+++ fppkgreposerver/trunk/sharedfpc/fprwebmodule.pas
@@ -315,7 +315,7 @@ begin
         StopProcessing := true;
         end;
       end
-    else
+    else if ARequest.Method='OPTIONS' then
       begin
       AResponse.Code := 403;
       AResponse.CodeText := GetStatusCode(AResponse.Code);
