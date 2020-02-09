cd3054d4e96cebb4885a2fb52119e571beb5f1fc
joost <joost@cnoc.nl>
2020-02-09 10:01:56 +0000

    * Log the backtrace in case of an exception
    git-svn-id: @3047 -

--- fppkgreposerver/trunk/sharedfpc/fprerrorhandling.pas
+++ fppkgreposerver/trunk/sharedfpc/fprerrorhandling.pas
@@ -21,6 +21,9 @@ implementation
 procedure fprOnShowRequestException(AResponse: TResponse; AnException: Exception; var handled: boolean);
 var
   ExcMessage: string;
+  BackTrace: string;
+  Frames: PPointer;
+  i: Integer;
 begin
   if AnException is EHTTP then
     begin
@@ -47,6 +50,18 @@ begin
     AResponse.Code := 500;
     AResponse.CodeText := GetStatusCode(AResponse.Code);
     end;
+
+  BackTrace := '';
+  if AnException<>nil then
+    BackTrace := 'Exception class: ' + Exception.ClassName + LineEnding + 'Message: ' + AnException.Message + LineEnding;
+  BackTrace := BackTrace + BacktraceStrFunc(ExceptAddr);
+  Frames := ExceptFrames;
+  for i := 0 to ExceptFrameCount -1 do
+    begin
+    BackTrace := BackTrace + LineEnding + BacktraceStrFunc(Frames[i]);
+    end;
+  TLogger.GetInstance.Error(BackTrace);
+
   TLogger.GetInstance.Trace('Respond with [' + IntToStr(AResponse.Code) + ':' + AResponse.CodeText + ']. Content: [' + Trim(Copy(AResponse.Content,1,100)) + ']');
   handled := True;
 end;
