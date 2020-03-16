e20d40f70812375ad376086b41fb92feb905103d
joost <joost@cnoc.nl>
2020-03-16 22:25:18 +0000

    * Fixed adaptation of configuration-file
    git-svn-id: @3102 -

--- fppkgreposerver/trunk/webclient/config/configure_and_run_nginx.sh
+++ fppkgreposerver/trunk/webclient/config/configure_and_run_nginx.sh
@@ -2,7 +2,7 @@
 envsubst '${HOST}${Buildagent_304_HOST}${Buildagent_320_HOST}${Buildagent_trunk_HOST}${Buildagent_fixes32_HOST}  ${Buildmanager_HOST}${Fppkgrepository_HOST}${Identityserver_HOST}${Repository_HOST}${Packagemanager_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 if [ $redirect_https == "no" ]
 then
-  sed -i '57,59d' /etc/nginx/conf.d/default.conf
+  sed -i '62,65d' /etc/nginx/conf.d/default.conf
 fi
 
 envsubst '${IdentityServerUrl}${WebClientUrl}${BuildAgentUrl}${PackageManagerUrl}${CategoryUrl}${BuildManagerUrl}${RepositoryUrl}${FppkgRepositoryUrl}' < /usr/share/nginx/html/assets/auth.clientConfiguration.json.template > /usr/share/nginx/html/assets/auth.clientConfiguration.json
